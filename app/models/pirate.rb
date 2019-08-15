befdb31b877152c443d0edf73a1c38acef1be5df
Rachel Grier <rgrier104@gmail.com>
2019-08-15 02:03:02 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
