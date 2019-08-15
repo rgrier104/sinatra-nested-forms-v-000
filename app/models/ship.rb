affe4fa22bdc12b24b1c7590fb4d65363cc2c125
Rachel Grier <rgrier104@gmail.com>
2019-08-15 02:04:22 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
+  end
+
+  
+
+  
 end
